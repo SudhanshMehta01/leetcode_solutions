7e111df806ad0d6a512b29f2a530730de5f16309
Sudhansh Mehta <sudhanshmehtavlogs@gmail.com>
2026-08-21 22:11:56 +0530

    Time: 35 ms (42.96%) | Memory: 59.1 MB (60.02%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,5 +1,57 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> list = new ArrayList<>();
+        int n = arr.length;
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = arr[i]+arr[left]+arr[right];
+                if(sum==0){
+                    list.add(Arrays.asList(arr[i],arr[left],arr[right]));
+                    left++;
+                    right--;
+                    while(left<right && arr[left]==arr[left-1])left++;
+                    while(left<right && arr[right]==arr[right+1])right--;
+                }
+                else if(sum<0) left++;
+                else right--;
+                
+                
+            }
+            
+        }
+        return list;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // Brute Force
         // Set<List<Integer>> list = new HashSet<>();
@@ -22,34 +74,34 @@ class Solution {
 
 
 
-        int n = arr.length;
-        Arrays.sort(arr);
-        List<List<Integer>> ans = new ArrayList<>();
-        // First loop for first element
-        for (int i = 0; i < n; i++) {
-            // Skip duplicates for first element
-            if (i > 0 && arr[i] == arr[i - 1]) continue;
-            // Two pointers
-            int left = i + 1, right = n - 1;
-            // Find pairs for current arr[i]
-            while (left < right) {
-                int sum = arr[i] + arr[left] + arr[right];
+        // int n = arr.length;
+        // Arrays.sort(arr);
+        // List<List<Integer>> ans = new ArrayList<>();
+        // // First loop for first element
+        // for (int i = 0; i < n; i++) {
+        //     // Skip duplicates for first element
+        //     if (i > 0 && arr[i] == arr[i - 1]) continue;
+        //     // Two pointers
+        //     int left = i + 1, right = n - 1;
+        //     // Find pairs for current arr[i]
+        //     while (left < right) {
+        //         int sum = arr[i] + arr[left] + arr[right];
 
-                if (sum == 0) {
-                    ans.add(Arrays.asList(arr[i], arr[left], arr[right]));
-                    left++;
-                    right--;
+        //         if (sum == 0) {
+        //             ans.add(Arrays.asList(arr[i], arr[left], arr[right]));
+        //             left++;
+        //             right--;
 
-                    // Skip duplicates for left
-                    while (left < right && arr[left] == arr[left - 1]) left++;
-                    // Skip duplicates for right
-                    while (left < right && arr[right] == arr[right + 1]) right--;
-                }
-                else if (sum < 0) left++;
-                else right--;
-            }
-        }
-        return ans;
+        //             // Skip duplicates for left
+        //             while (left < right && arr[left] == arr[left - 1]) left++;
+        //             // Skip duplicates for right
+        //             while (left < right && arr[right] == arr[right + 1]) right--;
+        //         }
+        //         else if (sum < 0) left++;
+        //         else right--;
+        //     }
+        // }
+        // return ans;
         
 
     }
