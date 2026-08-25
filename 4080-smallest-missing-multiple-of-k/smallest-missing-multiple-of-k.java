4ccdcfd6574f025d78740cede3741a2043ffa465
Sudhansh Mehta <sudhanshmehtavlogs@gmail.com>
2026-08-25 23:18:28 +0530

    Time: 3 ms (23.26%) | Memory: 45.7 MB (15.51%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,31 +1,29 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        // int n = nums.length;
-        // int min = Integer.MAX_VALUE;
-        // int ans=0;
+        int n = nums.length;
 
-        // HashSet<Integer> set = new HashSet<>();
-        // for(int i=0;i<n;i++){
-        //     set.add(nums[i]);
-        // }
-        // for(int i=1;i<=n;i++){
-        //     ans = k * i;
-        //     if(!set.contains(ans)) min= Math.min(min,ans);
-        // }
-        // return min;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(nums[i]);
+        }
+        for(int i=1;i<=n+1;i++){
+            int ans = k * i;
+            if(!set.contains(ans)) return ans;
+        }
+        return -1;
         //ye bhi nhi hua mwrpe gawar hu m
 
 
         
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            set.add(num);
-        }
-        int ans =k;
-        while(set.contains(ans)){
-            ans+=k;
-        }
-        return ans;
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int num : nums){
+        //     set.add(num);
+        // }
+        // int ans =k;
+        // while(set.contains(ans)){
+        //     ans+=k;
+        // }
+        // return ans;
        
     }
 }
