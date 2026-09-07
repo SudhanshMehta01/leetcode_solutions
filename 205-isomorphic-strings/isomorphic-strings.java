af607854966b7d44dc3e9a3a4aff5d44049e4a9d
Sudhansh Mehta <sudhanshmehtavlogs@gmail.com>
2026-09-08 00:25:11 +0530

    Time: 11 ms (65.72%) | Memory: 43.5 MB (93.57%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,9 +3,14 @@ class Solution {
 
         HashMap<Character, Character> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
+
+            //value dedo taki agge sirf a or b likh sake
             char a = s.charAt(i);
             char b = t.charAt(i);
+
+
             if (map.containsKey(a)) {
+                //sirf unequal h to hi false return karo
                 if (map.get(a) != b) {
                     return false;
                 }
@@ -14,6 +19,9 @@ class Solution {
                 if (map.containsValue(b)) {
                     return false;
                 }
+
+                // is condition ka testcase - s= "badc" , t = " baba"
+                //dekh lo dusre wala to nhi ja rha map m kisi or se judne
                 map.put(a, b);
             }
         }
